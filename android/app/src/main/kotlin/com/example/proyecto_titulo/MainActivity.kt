43c5addd64c8de1ab3d91205db8c61ffa99f1f46
Andoni <ignaziosusarte11@gmail.com>
2026-08-26 22:35:39 -0400

    Reorganiza proyecto MatchDay

--- android/app/src/main/kotlin/com/example/proyecto_titulo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.proyecto_titulo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/data/session.dart
+++
@@ -1,4 +0,0 @@
-import '../models/usuario.dart';
-
-Usuario? usuarioLogueado;
-
